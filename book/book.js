f4def3391636f1daa604b4213e13fb8b1d2a13cd
DerylJoeJohnson <deryljohnson@gmail.com>
2019-08-01 15:37:22 +0530

    better category

--- book/book.js
+++ book/book.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
-    var choice="";
-    $.ajax(
-    {
-        type:"GET",
-        url:"book.json",
-        success:function(list)
-        {
-            var options="<select id='opt'><option value='select'>---Select---</option>";
-            for (var j in list)
-            {
-                options+="<option value="+list[j].genre+">"+list[j].genre+"</option>";
-            }
-            options+="</select>";
-            $('.optionid').html(options);
-        }
-    });
-
-   
-
-
-    $("#1").click(function(){
+       $("#1").click(function(){
         $.ajax({
             type:"GET",
             url:"book.json",
@@ -34,13 +14,15 @@ $(document).ready(function(){
                     var description=data[i].descript;
                     var img=data[i].img;
                     var price=data[i].price;
-                    
+
+                   
                     if(genre==x){
-                        output+="<div class='col col-sm-4 col-4 d-flex align-items-stretch'><div class='card'> <img src='"+img+"'width=''height=''class='card-img-top'><div class='card-body'>"+name+"<br>"+author+"<br>"+genre+"<br>"+description+"<br>"+price+"<br></div></div></div>";  
+                        output+="<div class='col col-sm-4 col-4 d-flex align-items-stretch'><div class='card'> <img src='"+img+"'width='200px'height='150px'class='card-img-top'><div class='card-body'>"+name+"<br>"+author+"<br>"+genre+"<br>"+description+"<br>"+price+"<br></div></div></div>";  
                     }
                     if(x=="select"){ 
-                    output+="<div class='col col-sm-4 col-4 d-flex align-items-stretch'><div class='card'> <img src='"+img+"'width=''height=''class='card-img-top'><div class='card-body'>"+name+"<br>"+author+"<br>"+genre+"<br>"+description+"<br>"+price+"<br></div></div></div>";
+                    output+="<div class='col col-sm-4 col-4 d-flex align-items-stretch'><div class='card'> <img src='"+img+"'width='200px'height='150px'class='card-img-top'><div class='card-body'>"+name+"<br>"+author+"<br>"+genre+"<br>"+description+"<br>"+price+"<br></div></div></div>";
                     }
+                   
                     console.log(x);
                     console.log(data[i].genre);
                 }
